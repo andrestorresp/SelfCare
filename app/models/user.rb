b252fd039ec7bd671fa1bf5e6420f831727bdbd5
Gabriel Manrique <alberto.manrique99@gmail.com>
2024-03-06 16:55:41 -0400

    Validacion de sap presence y metodo privado si el usuario es doctor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :sap, presence: true, if: :is_doctor?
+
   validates :role, presence: true, inclusion: { in: %w(patient doctor)}
 
   scope :patient, -> { where("role = patient") }
   scope :doctor, -> { where("role = doctor") }
+
+  private
+
+  def is_doctor?
+    role == 'doctor'
+  end
 end
