8738856277914ad824baeefbe1d6e8953acc5c9e
andres torres <dragotorres16@gmail.com>
2024-03-07 17:09:43 -0400

    controladores de doctor casi listo

--- config/puma.rb
+++ config/puma.rb
@@ -11,8 +11,8 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
+# Specifies that the worker count should equal the number of processors in doctorion.
+if ENV["RAILS_ENV"] == "doctorion"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
