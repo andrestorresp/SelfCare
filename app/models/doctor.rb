8df6fd73c1ec5f62864a9a087e90b32a0b79637f
Gabriel Manrique <alberto.manrique99@gmail.com>
2024-03-11 14:12:16 -0400

    Dos cards en la visualizacion del perfil del paciente

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,8 +5,8 @@ class Doctor < ApplicationRecord
   validates :specialty, :sap, presence: true
   validates :sap, uniqueness: true
 
+  #Creación del método para mostrar el nombre completo de los doctores al ver las consultas
   def full_name
     "#{user.first_name} #{user.last_name}"
   end
-
 end
