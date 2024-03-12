bf2241cd6528aa2402ccbb768b9d6b3b69683ad7
Gabriel Manrique <alberto.manrique99@gmail.com>
2024-03-12 14:07:42 -0400

    Arreglos de los sedds

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,12 +5,8 @@ class Doctor < ApplicationRecord
   validates :specialty, :sap, presence: true
   validates :sap, uniqueness: true
 
-  #Creación del método para mostrar el nombre completo de los doctores al ver las consultas
+  # Método para mostrar el nombre completo de los doctores
   def full_name
     "#{user.first_name} #{user.last_name}"
   end
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
-
 end
