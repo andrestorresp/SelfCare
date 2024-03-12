e82739eb5c64599a7a140608a979ee6d23892fa2
Gabriel Manrique <alberto.manrique99@gmail.com>
2024-03-12 08:53:37 -0400

    Problemas con la consulta

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -17,10 +17,11 @@ class ConsultationsController < ApplicationController
     @patient = Patient.find(params[:patient_id])
     @doctor = current_user
     @consultation = Consultation.new
-    @consultation.doctor_id = @doctor.id
     @consultation.patient_id = @patient.id
+    @consultation.doctor_id = @doctor.id
     @consultation.diagnostic = params[:consultation][:diagnostic]
-    if @consultation.save!
+    puts "Donde esta el puuts?"+@consultation.inspect
+    if @consultation.save
       redirect_to patient_path(@patient)
     else
       render :new, status: :unprocessable_entity
@@ -29,9 +30,4 @@ class ConsultationsController < ApplicationController
 
   def update
   end
-
-
-
-
-
 end
