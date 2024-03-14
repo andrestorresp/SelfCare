dddcd33af83609395206904d7052de6b1fdbc1fb
Gabriel Manrique <alberto.manrique99@gmail.com>
2024-03-14 10:00:20 -0400

    Cambios de los mensajes al eliminar

--- app/javascript/notification.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('turbo:load', () => {
-  const notification = document.querySelector('.alert');
-  if (notification) {
-    setTimeout(() => {
-      notification.remove();
-    }, 5000); // Elimina la notificación después de 5 segundos (5000 milisegundos)
-  }
-});
